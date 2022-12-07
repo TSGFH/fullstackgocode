83a8dbd92700409088f5e31e9cb9a0d41d06d9a3
Boris Konak <boriskonak01@gmail.com>
2022-12-07 20:04:51 +0200

    finsihed assigment number 17

--- boris/main pc/JSDOM/main.js
+++ boris/main pc/JSDOM/main.js
@@ -19,7 +19,34 @@ function showMessage() {
   }
   let nums = [1,2,3,4,5,6,7,8,9,10];
 
-  const remover = document.addEventListener("",  function theremover(event){
-    
-    console.log(nums.pop())
-})
+  const remover = document.querySelector("input")
+  
+function theremover(){
+  nums.pop();
+  console.log(nums);
+}
+function theblack(){
+  document.body.style.background = "black";
+}
+function thered(){
+  document.body.style.background = "red";
+}
+function thegreen(){
+  document.body.style.background = "green";
+}
+function theblue(){
+  document.body.style.background = "blue";
+}
+
+setInterval(function changethebackground(){
+  let backgroundchanger = parseInt(Math.random()*4+1)
+  if(backgroundchanger === 1){
+    document.body.style.background = "yellow";
+  }else if(backgroundchanger === 2){
+    document.body.style.background = "pink";
+  }else if(backgroundchanger === 3){
+    document.body.style.background = "orange";
+  }else{
+    document.body.style.background = "brown";
+  }
+},200)
