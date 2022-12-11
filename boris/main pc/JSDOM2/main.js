b730e62f8d3d2fe9847f6957a46285d4dfb3d09b
Boris Konak <boriskonak01@gmail.com>
2022-12-11 21:19:09 +0200

    testing somthing it didnt work

--- boris/main pc/JSDOM2/main.js
+++ boris/main pc/JSDOM2/main.js
@@ -4,23 +4,29 @@
 // test.append(test2)
 
 const in1 = document.createElement("input");
-in1.setAttribute('type', 'text')
-in1.setAttribute('placeholder', 'first name')
+in1.setAttribute('type', 'text');
+in1.setAttribute('placeholder', 'first name');
+in1.setAttribute('name', 'fname');
 const in2 = document.createElement("input");
-in2.setAttribute('type', 'text')
-in2.setAttribute('placeholder', 'last name')
+in2.setAttribute('type', 'text');
+in2.setAttribute('placeholder', 'last name');
+in1.setAttribute('name', 'lname');
 const in3 = document.createElement("input");
-in3.setAttribute('type', 'text')
-in3.setAttribute('placeholder', 'city')
+in3.setAttribute('type', 'text');
+in3.setAttribute('placeholder', 'city');
+in1.setAttribute('name', 'city');
 const in4 = document.createElement("input");
-in4.setAttribute('type', 'text')
-in4.setAttribute('placeholder', 'country')
+in4.setAttribute('type', 'text');
+in4.setAttribute('placeholder', 'country');
+in1.setAttribute('name', 'country');
 const in5 = document.createElement("input");
-in5.setAttribute('type', 'text')
-in5.setAttribute('placeholder', 'date of birth')
+in5.setAttribute('type', 'text');
+in5.setAttribute('placeholder', 'date of birth');
+in1.setAttribute('name', 'dob');
 const in6 = document.createElement("input");
-in6.setAttribute('type', 'text')
-in6.setAttribute('placeholder', 'phone number')
+in6.setAttribute('type', 'text');
+in6.setAttribute('placeholder', 'phone number');
+in1.setAttribute('name', 'pnumber');
 document.body.append(in1,in2,in3,in4,in5,in6);
 
 const select1 = document.createElement("select");
@@ -42,30 +48,45 @@ const div1 = document.createElement("div");
 legend.append(div1);
 const in7 = document.createElement("input");
 in7.setAttribute("type","radio");
-in7.setAttribute("name","drone")
+in7.setAttribute("name","drone");
 div1.append(in7);
 const label1 = document.createElement("label");
-label1.innerText = "yes"
+label1.innerText = "yes";
 div1.append(label1);
 
 const div2 = document.createElement("div");
 legend.append(div2);
 const in8 = document.createElement("input");
 in8.setAttribute("type","radio");
-in8.setAttribute("name","drone")
+in8.setAttribute("name","drone");
 div2.append(in8);
 const label2 = document.createElement("label");
-label2.innerText = "no"
+label2.innerText = "no";
 div2.append(label2);
 
 const btn = document.createElement("button");
+btn.setAttribute("id","sender");
 btn.innerText = "submit";
 document.body.append(btn);
 
 
-
+const everything = document.querySelectorAll("input","option");
 const fullobj = {};
+const send = document.querySelector(".sender");
 
+everything.forEach(function(inp){
+    inp.addEventListener('keypress',function(e){
+        fullobj[e.target.name] = e.target.value;
+        if(e.charCode === 13){
+            send.click();
+        }
+    })
+});
 
-
-console.log(fullobj);
+send.addEventListener('click',function(){
+    if(!fullobj.fname||!fullobj.lname||!fullobj.city||!fullobj.country||!fullobj.dob||!fullobj.pnumber){
+        console.log("error");
+        return;
+    }
+    console.log(fullobj);
+}); 
