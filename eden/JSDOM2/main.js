802814952fed68d0bab40f2623b926a5592fa993
Predator7821 <sonix058@gmail.com>
2022-12-12 16:08:01 +0200

    jdsomtut

--- eden/JSDOM2/main.js
+++ eden/JSDOM2/main.js
@@ -5,7 +5,6 @@
 
 // mission 1 inputns
 const inputns = document.createElement("input")
-
 inputns.setAttribute("type","text")
 inputns.setAttribute("placeholder","first name")
 inputns.setAttribute("name","fname")
@@ -39,3 +38,79 @@ document.body.append(inputns,inputns2,inputns3,inputns4,inputns5,inputns6)
 
 // missions 2 select
 const select = document.createElement("select")
+select.setAttribute("name","gender")
+
+const option = document.createElement("option")
+option.setAttribute("value","1")
+option.innerText = ""
+
+const option2 = document.createElement("option")
+option2.setAttribute("value","male")
+option2.setAttribute("name","gendermale")
+option2.innerText = "male"
+
+const option3 = document.createElement("option")
+option3.setAttribute("value","female")
+option3.setAttribute("name","genderfemale")
+option3.innerText = "female"
+
+document.body.append(select)
+select.append(option,option2,option3)
+
+// missions 2 radio
+const radio = document.createElement("fieldset")
+document.body.append(radio)
+
+const legend = document.createElement("legend")
+legend.innerText = "Citizen Of israel:"
+radio.append(legend)
+
+const div = document.createElement("div")
+legend.append(div)
+
+const input7 = document.createElement("input")
+input7.setAttribute("type","radio")
+input7.setAttribute("name","Citizen Of israel")
+div.append(input7)
+
+const label = document.createElement("label")
+label.innerText = "yes"
+div.append(label)
+// no
+const div2 = document.createElement("div")
+legend.append(div2)
+
+const input8 = document.createElement("input")
+input8.setAttribute("type","radio")
+input8.setAttribute("name","Citizen Of israel")
+div2.append(input8)
+
+const label2 = document.createElement("label")
+label2.innerText = "no"
+div2.append(label2)
+
+// mission 3 submit
+const btn = document.createElement("button")
+btn.setAttribute("id","sender")
+btn.innerText = "Submit"
+document.body.append(btn)
+
+const selectors = document.querySelectorAll("*")
+
+const opbject = {}
+
+const send = document.querySelector("#sender")
+
+selectors.forEach(function(inp){
+    inp.addEventListener("keypress",function(e){
+        opbject[e.target.name] = e.target.value
+        if(e.charCode === 13){
+            send.click()
+
+        }
+    })
+})
+send.addEventListener("click",function(){
+    console.log(opbject)
+})
+
