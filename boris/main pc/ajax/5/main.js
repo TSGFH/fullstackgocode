24062aaf8ff5901c839d85b08940b8827e11b6e9
Boris Konak <boriskonak01@gmail.com>
2022-12-16 00:30:08 +0200

    changed ajax 5 slightly

--- boris/main pc/ajax/5/main.js
+++ boris/main pc/ajax/5/main.js
@@ -8,6 +8,7 @@ async function iwanttodie(){
         header.innerText = test2[i].id;
         document.body.append(header)    
     }
+    const timer = prompt("enter the amount of time you want to take to select the breed you want every 1000 is 1 second so think twice and do your math")
     setTimeout(async function changethebackground(){
         const picurl = "https://api.thecatapi.com/v1/images/search?breed_ids=";
         const breed = prompt("enter the breed type");
@@ -17,5 +18,5 @@ async function iwanttodie(){
         const apicture = document.createElement("img")
         apicture.setAttribute("src",test4[0].url)
         document.body.append(apicture)
-      },200)
+      },timer)
 }
