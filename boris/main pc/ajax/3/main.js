b3b249ea66f1c60e1c721db149b5b0c01ce32e27
Boris Konak <boriskonak01@gmail.com>
2022-12-15 21:17:36 +0200

    some more ajax

--- boris/main pc/ajax/3/main.js
+++ boris/main pc/ajax/3/main.js
@@ -1,7 +1,21 @@
-const url = "https://reqres.in/api/users/4"
+const url = "https://reqres.in/api/users"
 
 async function showuser(){
     const test = await fetch(url);
     const test2 = await test.json();
     console.log(test2);
+    for(let i = 0; i < test2.data.length; i++){
+        const pic1 = document.getElementById("1")
+        const pic2 = document.getElementById("2")
+        const pic3 = document.getElementById("3")
+        const pic4 = document.getElementById("4")
+        const pic5 = document.getElementById("5")
+        const pic6 = document.getElementById("6")
+        pic1.setAttribute("src",test2.data[0].avatar);
+        pic2.setAttribute("src",test2.data[1].avatar);
+        pic3.setAttribute("src",test2.data[2].avatar);
+        pic4.setAttribute("src",test2.data[3].avatar);
+        pic5.setAttribute("src",test2.data[4].avatar);
+        pic6.setAttribute("src",test2.data[5].avatar);
+    }
 }
