2741215cd1358ee36fe772a29142aa32724f2b31
Boris Konak <boriskonak01@gmail.com>
2022-12-16 00:25:15 +0200

    ajax is working aside from 5

--- boris/main pc/ajax/3/main.js
+++ boris/main pc/ajax/3/main.js
@@ -19,3 +19,86 @@ async function showuser(){
         pic6.setAttribute("src",test2.data[5].avatar);
     }
 }
+
+async function guy1(){
+    const test = await fetch(url);
+    const test2 = await test.json();
+
+    const g1n = document.getElementById("1stname")
+    const g1e = document.getElementById("1stmail")
+    g1n.innerText= test2.data[0].first_name + " " +test2.data[0].last_name;
+    g1e.innerText = test2.data[0].email;
+}
+
+async function guy2(){
+    const test = await fetch(url);
+    const test2 = await test.json();
+
+    const g1n = document.getElementById("1stname")
+    const g1e = document.getElementById("1stmail")
+    g1n.innerText= test2.data[1].first_name + " " +test2.data[1].last_name;
+    g1e.innerText = test2.data[1].email;
+}
+
+async function guy3(){
+    const test = await fetch(url);
+    const test2 = await test.json();
+
+    const g1n = document.getElementById("1stname")
+    const g1e = document.getElementById("1stmail")
+    g1n.innerText= test2.data[2].first_name + " " +test2.data[2].last_name;
+    g1e.innerText = test2.data[2].email;
+}
+async function guy4(){
+    const test = await fetch(url);
+    const test2 = await test.json();
+
+    const g1n = document.getElementById("1stname")
+    const g1e = document.getElementById("1stmail")
+    g1n.innerText= test2.data[3].first_name + " " +test2.data[3].last_name;
+    g1e.innerText = test2.data[3].email;
+}
+async function guy5(){
+    const test = await fetch(url);
+    const test2 = await test.json();
+
+    const g1n = document.getElementById("1stname")
+    const g1e = document.getElementById("1stmail")
+    g1n.innerText= test2.data[4].first_name + " " +test2.data[4].last_name;
+    g1e.innerText = test2.data[4].email;
+}
+async function guy6(){
+    const test = await fetch(url);
+    const test2 = await test.json();
+
+    const g1n = document.getElementById("1stname")
+    const g1e = document.getElementById("1stmail")
+    g1n.innerText= test2.data[5].first_name + " " +test2.data[5].last_name;
+    g1e.innerText = test2.data[5].email;
+}
+// Get DOM Elements
+const modal = document.querySelector("#my-modal");
+const modalBtn = document.querySelector("#modal-btn");
+const closeBtn = document.querySelector(".close");
+
+// Events
+modalBtn.addEventListener("click", openModal);
+closeBtn.addEventListener("click", closeModal);
+window.addEventListener("click", outsideClick);
+
+// Open
+function openModal() {
+  modal.style.display = "block";
+}
+
+// Close
+function closeModal() {
+  modal.style.display = "none";
+}
+
+// Close If Outside Click
+function outsideClick(e) {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+}
