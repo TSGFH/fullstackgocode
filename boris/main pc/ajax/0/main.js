6d2382081e402e384678bf1ba19dead5cb26117c
Boris Konak <boriskonak01@gmail.com>
2022-12-16 11:34:32 +0200

    finished ajax

--- boris/main pc/ajax/0/main.js
+++ boris/main pc/ajax/0/main.js
@@ -1,42 +1,44 @@
-const url = " https://randomuser.me/api/ ";
+const url = "https://randomuser.me/api/";
 
-let avatar = document.getElementById("avatar");
-let fullname = document.getElementById("fullname");
-let username = document.getElementById("username");
-let email = document.getElementById("email");
-let city = document.getElementById("city");
-let btn = document.getElementById("btn");
+document.body.style.backgroundColor = "pink"
 
-btn.addEventListener("click", function () {
-  fetch(url)
-    .then(handleErrors)
-    .then(parseJSON)
-    .then(updateProfile)
-    .catch(printError);
-});
+async function showUser(){
+  const test = await fetch(url);
+  const test2 = await test.json();
+  console.log(test2.results[0]);
+  
+  const div = document.getElementById("div1");
+  const profilepic = document.createElement("img");
+  profilepic.setAttribute("src",test2.results[0].picture.large);
+  div.append(profilepic);
 
-function handleErrors(res) {
-  if (!res.ok) {
-    throw error(res.status);
-  }
-  console.log(res);
-  return res;
-}
+  const fullname = test2.results[0].name.title + " " + test2.results[0].name.first + " " + test2.results[0].name.last;
+  const p1 = document.createElement("p");
+  p1.innerText = fullname;
+  div.append(p1);
 
-function parseJSON(res) {
-  return res.json();
-}
+  const fulldob = "age: " + test2.results[0].dob.age + " date of birth: " + test2.results[0].dob.date
+  const p2 = document.createElement("p");
+  p2.innerText = fulldob;
+  div.append(p2);
 
-function updateProfile(profile) {
-  avatar.src = profile.results[0].picture.medium;
-  fullname.innerHTML =
-    profile.results[0].name.first + " " + profile.results[0].name.last;
-  username.innerHTML = profile.results[0].login.username;
-  email.innerHTML = profile.results[0].email;
-  city.innerHTML = profile.results[0].location.city;
-  return 1;
-}
+  const cellpnum = "cell number: " + test2.results[0].cell
+  const p3 = document.createElement("p");
+  p3.innerText = cellpnum;
+  div.append(p3);
 
-function printError(error) {
-  console.log(error);
+  const mail = "email: " + test2.results[0].email
+  const p4 = document.createElement("p");
+  p4.innerText = mail;
+  div.append(p4);
+
+  const gen = "gender: " + test2.results[0].gender
+  const p5 = document.createElement("p");
+  p5.innerText = gen;
+  div.append(p5);
+  
+  const fullid = "id: " + test2.results[0].id.name + " value: "  + test2.results[0].id.value
+  const p6 = document.createElement("p");
+  p6.innerText = fullid;
+  div.append(p6);
 }
--- boris/main pc/ajax/5/main.js
+++ boris/main pc/ajax/5/main.js
@@ -3,20 +3,39 @@ const superurl = "https://api.thecatapi.com/v1/breeds";
 async function iwanttodie(){
     test1 = await fetch(superurl);
     test2 = await test1.json();
+    const selector = document.createElement("select")
+    const diver = document.getElementById("mydiv")
+    diver.append(selector)
     for(let i = 0; i < test2.length; i++){
-        const header = document.createElement("p");
+        const header = document.createElement("option");
         header.innerText = test2[i].id;
-        document.body.append(header)    
+        selector.append(header)    
     }
-    const timer = prompt("enter the amount of time you want to take to select the breed you want every 1000 is 1 second so think twice and do your math")
-    setTimeout(async function changethebackground(){
-        const picurl = "https://api.thecatapi.com/v1/images/search?breed_ids=";
-        const breed = prompt("enter the breed type");
-        const newurl = picurl+breed;
-        test3 = await fetch(newurl)
-        test4 = await test3.json();
-        const apicture = document.createElement("img")
-        apicture.setAttribute("src",test4[0].url)
-        document.body.append(apicture)
-      },timer)
 }
+async function changethebackground(){
+    const picurl = "https://api.thecatapi.com/v1/images/search?breed_ids=";
+    const breed = prompt("enter the breed type");
+    const newurl = picurl+breed;
+    test3 = await fetch(newurl)
+    test4 = await test3.json();
+    const apicture = document.createElement("img")
+    apicture.setAttribute("src",test4[0].url)
+    document.body.append(apicture)
+
+    const everything = document.querySelectorAll('*');
+    const fullobj = {};
+    const send = document.querySelector(".sender");
+
+everything.forEach(function(inp){
+inp.addEventListener('keypress',function(e){
+    fullobj[e.target.name] = e.target.value;
+    if(e.charCode === 13){
+        send.click();
+    }
+})
+});
+
+send.addEventListener('click',function(){
+console.log(fullobj);
+});
+  }
