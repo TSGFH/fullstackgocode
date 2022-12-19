f2e83be60ea10f26457697fb3ce0b254ebd03c92
Boris Konak <boriskonak01@gmail.com>
2022-12-19 19:16:27 +0200

    es6

--- boris/main pc/ES6/main.js
+++ boris/main pc/ES6/main.js
@@ -1,11 +1,11 @@
 function detectCollision(objects, point) {
   for (let i = 0; i < objects.length; i++) {
-    let object = objects[i];
+    const { x, y, width, height } = objects[i];  // Here we are destructuring each object
     if (
-      point.x >= object.x &&
-      point.x <= object.x + object.width &&
-      point.y >= object.y &&
-      point.y <= object.y + object.height
+      point.x >= x && // notice that we no longer need to use the object.x we can simply use the new variables
+      point.x <= x + width &&
+      point.y >= y &&
+      point.y <= y + height
     )
       return object;
   }
