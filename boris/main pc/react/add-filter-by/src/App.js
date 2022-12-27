4a934b90c8f515481554eefad3d1f49e5cd4b4fe
Boris Konak <boriskonak01@gmail.com>
2022-12-27 22:24:30 +0200

    sorting with react

---
+++ boris/main pc/react/add-filter-by/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Card from './components/Card'
+import Filter from './components/Filter'
+
+function App() {
+  return (
+    <div className="App">
+      <Filter />
+      <Card />
+    </div>
+  )
+}
+
+export default App;
